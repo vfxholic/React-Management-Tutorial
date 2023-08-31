7d36043a3437fe4b1b86efabb06b354dc215e288
vfxhollic <babei123@naver.com>
2023-08-31 17:33:04 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://picsum.photos/id/254/100/100',
+  'name': '유승걸',
+  'birthday': '930316',
+  'gender': 'female',
+  'job': 'engineer'
+  },
+  {
+    'id': 2,
+    'image': 'https://picsum.photos/id/253/100/100',
+    'name': '전소현',
+    'birthday': '930316',
+    'gender': 'female',
+    'job': 'engineer'
+  },
+  {
+    'id': 3,
+    'image': 'https://picsum.photos/id/252/100/100',
+    'name': '유지후',
+    'birthday': '930316',
+    'gender': 'female',
+    'job': 'engineer'
+  }
+]
+
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
